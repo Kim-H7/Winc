21499a61b3a18b169f3f382023149a1f860f1c07
Kim Hoogendoorn <98826442+Kim-H7@users.noreply.github.com>
2022-02-10 16:38:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ Scorer_1 = "Ruud Gullit"
 Scorer_2 = "Marco van Basten"
 goal_0 = 32
 goal_1 = 54
-scorers = (Scorer_1) + (' ') + str(goal_0), (Scorer_2) + (' ') + str(goal_1)
-Report = (Scorer_1) +' scored in the '+ str(goal_0) +'nd minute', (Scorer_2) + ' scored in the '+ str(goal_1)+ 'th minute'
+scorers = (Scorer_1) + (' ') + str(goal_0) + ', ' + (Scorer_2) + (' ') + str(goal_1)
+report = (Scorer_1) + ' scored in the '+ str(goal_0) +'nd minute\n' + (Scorer_2) + ' scored in the ' + str(goal_1) + 'th minute'
+print(report)
 
 #part 2
 player = 'Ronald Koeman'
-first_name = player.find('Ronald'), player[:6]
-last_name_len = player.find('Koeman'), len(player[7:]), player[7:]
-name_short = player[0] + player[7:]
-chant = (player[:6] + '! ') * 2 + (player[:6] + '!')
-good_chant = (2 != 3)
-
-"""Ik kom niet door de check van wincpy heen omdat er nog ' ' om de strings heen staan in het resultaat van lijn 12. Ik heb lang gezocht
-en geprobeerd maar ik kom er niet goed uit. Is daar een manier voor? In ieder geval alvast bedankt voor de feedback!"""
+first_name = player[:player.find(' ')]
+last_name_len = len(player[player.find(' ') + 1:])
+name_short = player[0] + ('. ') + player[player.find(' ') + 1:]
+chant = (first_name + '! ') * 5 + (first_name + '!')
+good_chant = (chant[:-1] != (' '))
+print (good_chant)
